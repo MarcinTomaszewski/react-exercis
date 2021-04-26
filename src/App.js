1e4491112d2569ee0e80da259d3445e1193e1afa
MarcinTomaszewski <marcin.tomaszewski7@wp.pl>
2021-04-26 09:38:12 +0200

    Using useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
-export default function App() {
+const App = () => {
+  const [value, setValue] = useState("");
+  // funkcje asynchroniczne - handleChangeInput, handleButtonClick
+  const handleChangeInput = e => setValue(e.target.value);
+
+  const handleButtonClick = () => setValue("");
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <React.Fragment>
+      <input
+        value={value}
+        onChange={handleChangeInput}
+        placeholder="Wpisz..."
+        type="text"
+      />
+      <button onClick={handleButtonClick}>Reset</button>
+      <h1>{value}</h1>
+    </React.Fragment>
   );
-}
+};
+
+export default App;
